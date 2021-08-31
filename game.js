96006f655ee4dfe238aa31a07e0d73bf7ffbeda8
Simran Kaur <kaursim.2019@yahoo.com>
2021-08-30 22:01:07 -0700

    complete start over

---
+++ game.js
@@ -0,0 +1,133 @@
+const question = document.querySelector('#question');
+
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+
+// const timer = document.querySelector('#timer');
+
+var gameOverEl = document.querySelector("#start")
+let currentQuestion = {}
+let acceptingAnswers = true
+let timer = 120
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: "Commonly used data types DO NOT include",
+
+        choice1: "strings",
+        choice2: "booleans",
+        choice3: "alerts",
+        4: "numbers",
+        correctAnswer: "3",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with: ",
+
+        choice1: "quotes",
+        choice2: "curly brackets",
+        choice3: "parentheses",
+        choice4: "square brackets",
+        answer: "3",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+
+        choice1: "commas",
+        choice2: "curly brackets",
+        choice3: "quotes",
+        choice4: "parentheses",
+        answer: "3",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store: ",
+
+        choice1: "numbers and strings",
+        choice2: "other arrays",
+        choice3: "booleans",
+        choice4: "all of the above",
+        answer: "4",
+    },
+    {
+        question: "A very usefil tool used during development and debugging for printing content to the debugger is:",
+
+        choice1: "JavaScript",
+        choice2: "terminal/bash",
+        choice3: "for loops",
+        choice4: "console log",
+        answer: "4",
+    }
+]
+
+const SC0RE_POINTS = 10
+const MAX_QUESTIONS = 5
+
+startGame = () => {
+    questionCounter = 0
+    score = 120
+    availableQuestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+        return window.location.assign('/end.html')
+    }
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+    const questionsIndex = Math.floor(Math.random()* availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice[questionsIndex, 1]
+
+    acceptingAnswers = true
+
+}
+
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false 
+        const selectedChoice = e.target 
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'corect' : 'incorrect'
+
+        if(classToApply === 'incorrect') {
+            decrementScore(SC0RE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion
+        }, 1000)
+    })
+})
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        timer--;
+        gameOverEl.textContent = timer + " seconds left till game over.";
+        if (timer === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+gameOverEl.addEventListener('click', function () {
+    setTime();
+    console.log("clicked")
+    console.log
+})
--- script.js
+++ script.js
@@ -19,83 +19,118 @@
 //change the css on all of the required html elements so that you get what we want on the page 
 //display the first object in the array on the page 
 //set up the handlers on the answer buttons so that when a user selects an answer we can check a few things 
-    //check if the answer is right or wrong => increment counter to the next question => if we are at the end of our array we want to end the quiz => incorrect answer decrement time increment counter 
-    //show the user if they got the question correct or incorrect underneath the answer options => set timeout to change classes to show the user right or wrong
+//check if the answer is right or wrong => increment counter to the next question => if we are at the end of our array we want to end the quiz => incorrect answer decrement time increment counter 
+//show the user if they got the question correct or incorrect underneath the answer options => set timeout to change classes to show the user right or wrong
 //at the end of the quiz hide the questions and show the user score (created this var earlier), use the score and the user initials to save that info in local storage
-    //[{user: name, score: 10}] (stringify and store multiple things in local storage) each time we pull the info from local storage we set our highscores to either the info there || [] (an empty array )
+//[{user: name, score: 10}] (stringify and store multiple things in local storage) each time we pull the info from local storage we set our highscores to either the info there || [] (an empty array )
 
 //hide the user input screen and show the highscores to the user and ask if they want to play again 
 
 
-
+var counter
+var score
+var highScore
 
 var questions = [
-    {question: "Commonly used data types DO NOT include"
-    answers: {
-        a1: "strings"
-        a2: "booleans"
-        a3: "alerts"
-        a4: "numbers"
+    {
+        question: "Commonly used data types DO NOT include",
+        answers: {
+            1: "strings",
+            2: "booleans",
+            3: "alerts",
+            4: "numbers",
+        },
+        correctAnswer: "3",
     },
-    correctAnswer: "a3"
-},
-    {question: "The condition in an if/else statement is enclosed with: "
-    answers: {
-    a1: "quotes"
-    a2: "curly brackets"
-    a3: "parentheses"
-    a4: "square brackets"
-    }, 
-    correctAnswer: "a3"
-    }
-    {question: "String values must be enclosed within ______ when being assigned to variables."
-    answers: {
-    a1: "commas"
-    a2: "curly brackets"
-    a3: "quotes"
-    a4: "parentheses"
+    {
+        question: "The condition in an if/else statement is enclosed with: ",
+        answers: {
+            1: "quotes",
+            2: "curly brackets",
+            3: "parentheses",
+            4: "square brackets",
+        },
+        correctAnswer: "3",
     },
-    correctAnswer: "a3"
-    }
-    {question: "Arrays in JavaScript can be used to store: "
-    answers: {
-    a1: "numbers and strings"
-    a2: "other arrays"
-    a3: "booleans"
-    a4: "all of the above"
-    }, 
-    correctAnswer: "a4"
-    }
-    {question: "A very usefil tool used during development and debugging for printing content to the debugger is:"
-    answers: {
-    a1: "JavaScript"
-    a2: "terminal/bash"
-    a3: "for loops"
-    a4: "console log"
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        answers: {
+            1: "commas",
+            2: "curly brackets",
+            3: "quotes",
+            4: "parentheses",
+        },
+        correctAnswer: "3",
     },
-    correctAnswer: "a4"
+    {
+        question: "Arrays in JavaScript can be used to store: ",
+        answers: {
+            1: "numbers and strings",
+            2: "other arrays",
+            3: "booleans",
+            4: "all of the above",
+        },
+        correctAnswer: "4",
+    },
+    {
+        question: "A very usefil tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            1: "JavaScript",
+            2: "terminal/bash",
+            3: "for loops",
+            4: "console log",
+        },
+        correctAnswer: "4",
     }];
 
 
 
 
-var gameOverEl = document.querySelector("#timer")
+var gameOverEl = document.querySelector("#start")
 var secondsLeft = 60;
-gameOverEl.addEventListener('click', function (event) {
 
 function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      gameOverEl.textContent = secondsLeft + " seconds left till game over.";
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        gameOverEl.textContent = secondsLeft + " seconds left till game over.";
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
     }, 1000);
-  }
-  function sendMessage() {
-    gameOverEl.textContent = "Game Over!";
-  }
-  setTime();
+}
+gameOverEl.addEventListener('click', function () {
+    setTime();
+    console.log("clicked")
+    console.log
 })
 
+function sendMessage() {
+    gameOverEl.textContent = "Game Over!";
+}
+
+var questionsEl = document.createElement("div")
+// questionsEl.style.display = "none"
+console.log();
+
+var displayQ = document.createElement("h1")
+displayQ.textContent = questions[0].question;
+questionsEl.appendChild(displayQ);
+console.log(displayQ);
+console.log(questions[0].question)
+document.body.appendChild(questionsEl);
+
+
+function displayQuestion() {
+    // for (let i = 0; i < questions.length; i++) {
+    //     displayQ
+        for (let index = 0; index < 4; index++) {
+        var answerBtn = document.createElement("button")
+        answerBtn.textContent = questions[0].answers[index + 1];
+        questionsEl.appendChild(answerBtn);
+        
+    }
+}
+// }
+
+displayQuestion; 
